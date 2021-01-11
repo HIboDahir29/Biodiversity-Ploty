4223b91df0e9b63e210edfcff1c66aa0c30a89b1
Hibo <lm7024an@go.minnstate.edu>
2021-01-11 15:48:19 -0600

    Code Updated

---
+++ Biodiversity/static/js/app.js
@@ -0,0 +1,34 @@
+// d3.json("samples.json").then(function (sample) {
+//     console.log(sample);
+
+//     var ids = sample.names;
+//     console.log(ids);
+
+//     d3.select("#selDataset")
+//         .selectAll("option")
+//         .sample(ids)
+//         .enter()
+//         .append("option")
+//         .html(function (d) {
+//             return `<option>${d}</option`;
+//         });
+
+// });
+
+
+d3.json("samples.json").then(function createPlotly(data) {
+    console.log(data);
+    var testid = data.names;
+    console.log(testid);
+
+    // Create a dynamic dropdown menu
+    d3.select("#selDataset")
+        .selectAll("option")
+        .data(testid)
+        .enter()
+        .append("option")
+        .html(function (d) {
+            return `<option>${d}</option`;
+        });
+
+});
